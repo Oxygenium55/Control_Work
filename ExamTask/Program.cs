fa627f34282224e8aad73bbee21f03efff50b56b
Oxygen <grandnorik@mail.ru>
2022-12-06 17:21:37 +0600

    task solved

--- ExamTask/Program.cs
+++ ExamTask/Program.cs
@@ -3,4 +3,30 @@
 с клавиатуры, либо задать на старте выполнения алгоритма.
 */
 
+void TransformArray(string[] array1, string[] array2)
+{
+    int index = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[index] = array1[i];
+        index++;
+        }
+    }
+}
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] baseArray = new string[8] {"wow", "2022", "hello", "world", "!", ":-)", "-17", "C"};
+string[] resultArray = new string[baseArray.Length];
+TransformArray(baseArray, resultArray);
+PrintArray(baseArray);
+PrintArray(resultArray);
